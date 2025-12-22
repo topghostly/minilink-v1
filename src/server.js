ad3a82208f994a29eb27b999a1a176ba156b8ada
= <topghostly@gmail.com>
2025-12-22 19:33:32 +0100

    feat: added basic ci script

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || process.env.DEFAULT_PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
