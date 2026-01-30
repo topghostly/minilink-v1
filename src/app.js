8ce3476b824a89b56675cc88730363b12e41c094
= <topghostly@gmail.com>
2026-01-30 18:10:34 +0100

    fix: merge conflict

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ app.get("/health", (req, res) => {
       status: "healthy",
       version: process.env.API_VERSION,
       environment: process.env.NODE_ENV,
-      pod_name: process.env.POD_NAME,
+      // pod_name: process.env.POD_NAME,
     },
     error: null,
     meta: {
